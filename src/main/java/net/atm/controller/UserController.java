46cab61491c196d07a744844683e049d2e7c8bfd
Mohammed Thaabit Jacobs <mohammedthaabitjacobs@gmail.com>
2020-11-03 16:01:36 +0200

    userCon

--- src/main/java/net/atm/controller/UserController.java
+++ src/main/java/net/atm/controller/UserController.java
@@ -1,10 +1,12 @@
 package net.atm.controller;
 
 import static spark.Spark.*;
+import static net.atm.util.JsonUtil.*;
+
 import net.atm.service.user.UserService;
 
 public class UserController {
     public UserController(final UserService userService){
-        get("/users", (request, response) -> userService.selectAllUser());
+        get("/users", (request, response) -> userService.selectAllUser(), json());
     }
 }
